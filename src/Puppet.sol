80899365bb1d161d21d975ab21e4cd2f4c8e3a5f
johnnyonline <johnnyonline.eth@gmail.com>
2023-04-14 18:28:43 +0300

    ...

--- src/Puppet.sol
+++ src/Puppet.sol
@@ -21,7 +21,91 @@ contract Puppet {
     // 4. investor can withdraw funds only if trader has closed the position
 
     // questions:
-    // require the trader's collateral to be x% of the total funds deposited by investors? (e.g. in order to use 1000$ of investors funds, trader will need 10% so 100$)
-    // cut trader collateral on loss? (to make sure incentives are alligned on all scenarios)
-    // limit performance fee to x% of TVL? (to disincentivize over risk taking)
+    // 1. require the trader's collateral to be x% of the total funds deposited by investors? (e.g. in order to use 1000$ of investors funds, trader will need 10% so 100$)
+    // 2. cut trader collateral on loss?
+    // 3. limit performance fee to x% of TVL? (to disincentivize over risk taking)
+    //
+    // one of the first 2 is a must in order to allign incentives on all scenarios. ideally both. but probably only #1 makes sense here
+    // #3 might not make sense here
+
+    struct Vault {
+        uint256 totalAssets;
+        uint256 totalShares;
+        bool isPositionOpen;
+        bool isActive;
+    }
+
+    // trader => token => vault
+    // mapping (address => mapping (address => Vault)) tradersLongVaults;
+    // mapping (address => mapping (address => Vault)) tradersShortVaults;
+
+    // trader => token => isLong => vault
+    mapping (address => mapping (address => mapping (bool => Vault))) traderVaults;
+
+    // investor => trader => token => isLong => shares
+    mapping (address => mapping (address => mapping (bool => uint256))) investorShares;
+    
+    mapping(address => mapping(address => uint256)) public investors;
+
+    function registerVault(address _trader, address _token, bool _isLong) external {
+        if (traderVaults[_trader][_token][_isLong].isActive) revert(); // vault already registered
+        if (!tokenWhitelist[_token]) revert();
+        
+        traderVaults[_trader][_token][_isLong].isActive = true;
+    }
+
+    function deposit(address[] _traders, uint256[] _amounts, address _token, address _receiver, bool _isLong) external {
+        if (_traders.length != _amounts.length) revert(); // arrays not the same length
+
+        uint256 totalAmount;
+        for (uint256 i = 0; i < _traders.length; i++) {
+            _deposit(_traders[i], _token, msg.sender, _receiver, _amounts[i], _isLong);
+            totalAmount += _amounts[i];
+        }
+
+        IERC20(_token).transferFrom(msg.sender, address(this), totalAmount);
+    }
+
+    //
+    // -------------------------------------- INTERNAL --------------------------------------
+    //
+    
+    function _deposit(address _trader, address _token, address _caller, address _receiver, uint256 _assets, bool _isLong) internal override {
+        Vault storage _vault = traderVaults[_trader][_token][_isLong];
+
+        if (!_vault.isActive) revert();
+        if (_vault.isPositionOpen) revert();
+        if (pauseDeposit) revert DepositPaused();
+        if (_receiver == address(0)) revert ZeroAddress();
+        if (!(_assets > 0)) revert ZeroAmount();
+
+        uint256 _shares = convertToShares(_vault.totalAssets, _vault.totalShares, _assets);
+        if (!(_shares > 0)) revert ZeroAmount();
+
+        investorShares[_receiver][_trader][_token][_isLong] += _shares;
+
+        _vault.totalShares += _shares;
+        _vault.totalAssets += _assets;
+
+        emit Deposit(_trader, _token, _isLong, _caller, _receiver, _assets, _shares);
+    }
+
+    // function _withdraw(address _caller, address _receiver, address _owner, uint256 _assets, uint256 _shares) internal override {
+    //     if (settings.pauseWithdraw) revert WithdrawPaused();
+    //     if (_receiver == address(0)) revert ZeroAddress();
+    //     if (_owner == address(0)) revert ZeroAddress();
+    //     if (!(_shares > 0)) revert ZeroAmount();
+    //     if (!(_assets > 0)) revert ZeroAmount();
+        
+    //     if (_caller != _owner) {
+    //         uint256 _allowed = allowance[_owner][_caller];
+    //         if (_allowed < _shares) revert InsufficientAllowance();
+    //         if (_allowed != type(uint256).max) allowance[_owner][_caller] = _allowed - _shares;
+    //     }
+        
+    //     _burn(_owner, _shares);
+    //     totalAUM -= _assets;
+        
+    //     emit Withdraw(_caller, _receiver, _owner, _assets, _shares);
+    // }
 }
