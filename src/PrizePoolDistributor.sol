73a10713c9c311d0d9ae8fe175072f74c3f9197a
johnnyonline <johnnyonline.eth@gmail.com>
2023-05-08 18:43:57 +0300

    ...

--- src/PrizePoolDistributor.sol
+++ src/PrizePoolDistributor.sol
@@ -2,5 +2,14 @@
 pragma solidity 0.8.17;
 
 contract PrizePoolDistributor {
-    // TODO
+    
+    // TODO 
+
+    // ============================================================================================
+    // Receive Function
+    // ============================================================================================
+
+    receive() external payable {
+        if (orchestrator.getReferralRebatesSender() == msg.sender) payable(orchestrator.getPrizePoolDistributor()).sendValue(msg.value);
+    }
 }
